ea4dbe0ff9d6a2e95edcff4e8a2ec757a490eb62
VJubert <val_jub@live.fr>
2018-03-31 11:32:11 +0200

    fix joueur

--- client.py
+++ client.py
@@ -3,34 +3,34 @@ import asyncio
 import json
 import random
 import sys
-import argparse²
+import argparse
 
-class Joueur :
-    #pile des positions
-    positions=[]
-    #0 nord / 1 est / 2 sud / 3 ouest
-    direction=0
-    id=0
-    score=0
+
+class Joueur:
+    # pile des positions
+    positions = []
+    # 0 nord / 1 est / 2 sud / 3 ouest
+    direction = 0
+    id = 0
+    score = 0
 
     def __init__(self, id, pos, rot):
-        self.id=id;
-        self.positions.insert(0,pos)-
-        if rot == [0,1]:
-            self.direction=3
-        else if rot == [0,-1]:
-            self.direction=1
-        else if rot == [1,0]:
-            self.direction=2
-        else if rot == [-1,0]:
-            self.direction=0
+        self.id = id
+        self.positions.insert(0, pos)
+        if rot == [0, 1]:
+            self.direction = 3
+        elif rot == [0, -1]:
+            self.direction = 1
+        elif rot == [1, 0]:
+            self.direction = 2
+        elif rot == [-1, 0]:
+            self.direction = 0
 
     def __init__(self, id):
-        self.id=id;
+        self.id = id
 
     def update(self, position):
-        self.score=score;
-        self.positions.insert(0,position):
+        self.positions.insert(0, position)
 
     def is_safe(self):
         False
@@ -44,7 +44,7 @@ class ClientConcoursProg(asyncio.Protocol):
         self.liste_actions = [("move", "shoot"), ("move",),
                               ("move",), ("move",), ("hrotate",)]
         self.monde = {}
-        self.joueurs={}
+        self.joueurs = {}
         self.idJoueur = -1
 
     def connection_made(self, transport):
@@ -56,23 +56,16 @@ class ClientConcoursProg(asyncio.Protocol):
         for d in data.decode().strip().split("\n"):
             self.traite_donnees(json.loads(d))
 
-    def update_or_create_joueur(self, joueurs):
-        for (id, pos) in joueurs:
-            if id in self.joueurs:
-                joueur[id].update(pos)
-            else :
-                self.joueurs[id]=Joueur(id, pos)
-
     def traite_donnees(self, donnees):
         if "idJoueur" in donnees:
             self.idJoueur = donnees["idJoueur"]
-            self.joueurs[id]=Joueur(self.idJoueur)
+            self.joueurs[id] = Joueur(self.idJoueur)
         if "map" in donnees:
             self.monde = donnees
             print("Nombre d'objets dans la carte :" + str(len(donnees["map"])))
             print("Joueurs :" + str(donnees["joueurs"]))
             for j in donnees["joueurs"]:
-                if(j["id"] == self.idJoueur):
+                if (j["id"] == self.idJoueur):
                     print("Position :" + str(j["position"]))
         action = self.liste_actions[self.state]
         self.state = (self.state + 1) % len(self.liste_actions)
@@ -88,7 +81,7 @@ class ClientConcoursProg(asyncio.Protocol):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="IA concours de programmation")
     parser.add_argument("hostname", help="Nom ou adresse IP du serveur de jeu")
-    parser.add_argument("-p", dest="port", default=8889,type=int, help="Port du serveur auquel se connecter")
+    parser.add_argument("-p", dest="port", default=8889, type=int, help="Port du serveur auquel se connecter")
     args = parser.parse_args()
     loop = asyncio.get_event_loop()
 
@@ -97,3 +90,4 @@ if __name__ == '__main__':
     loop.run_until_complete(coro)
     loop.run_forever()
     loop.close()
+
