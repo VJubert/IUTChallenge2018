ba5afd7c0055f03e7a7f3674220f85c584cbd372
VJubert <val_jub@live.fr>
2018-04-01 12:21:53 +0200

    clean code and start astar

--- client.py
+++ client.py
@@ -5,11 +5,7 @@ import random
 import sys
 import argparse
 
-import Map
 from game import Game
-from joueur import Joueur, cast_rot_inverse
-from PathFinding import *
-from moves import *
 
 
 class ClientConcoursProg(asyncio.Protocol):
@@ -17,14 +13,9 @@ class ClientConcoursProg(asyncio.Protocol):
     def __init__(self, loop):
         self.game = Game()
         self.loop = loop
-        self.state = 0
-        self.liste_actions = [("move", "shoot"), ("move",),
-                              ("move",), ("move",), ("hrotate",)]
-        self.monde = {}
-        self.map = None
-        self.joueurs = {}
-        self.projectiles = {}
-        self.idJoueur = -1
+        # self.state = 0
+        # self.liste_actions = [("move", "shoot"), ("move",),
+        #                       ("move",), ("move",), ("hrotate",)]
 
     def connection_made(self, transport):
         self.transport = transport
@@ -35,144 +26,12 @@ class ClientConcoursProg(asyncio.Protocol):
             self.traite_donnees(json.loads(d))
 
     def traite_donnees(self, donnees):
-        self.send_message(self.game.play(donnees))
-        # # print("         NEW TURN            ")
-        # # print(self.joueurs)
-        # # print(self.projectiles)
-        # # print(donnees)
-        # if "idJoueur" in donnees:
-        #     self.idJoueur = donnees["idJoueur"]
-        #     self.joueurs[self.idJoueur] = Joueur(self.idJoueur, None, None)
-        #     return
-        # if "map" in donnees:
-        #     self.monde = donnees
-        #     self.map = Map.Map(donnees)
-        #     for j in donnees["joueurs"]:
-        #         id = j["id"]
-        #         position = j["position"]
-        #         direction = j["direction"]
-        #         if id in self.joueurs:
-        #             self.joueurs[id].update(position, direction)
-        #         else:
-        #             self.joueurs[id] = Joueur(id, position, direction)
-        #     return
-        #
-        # for msg in donnees:
-        #     if "projectile" in msg:
-        #         action = msg[1]
-        #         id = msg[2]
-        #         if "move" == action:
-        #             newPos = msg[3]
-        #             (pos, dir) = self.projectiles[id]
-        #             self.projectiles[id] = (newPos, dir)
-        #         if "explode" == action:
-        #             pos = msg[3]
-        #             posList = msg[4]
-        #             tankDone = msg[5]
-        #             cell = self.map.get_at(*pos)
-        #             if tankDone:
-        #                 j = cell.joueur
-        #                 j.positions = []
-        #                 cell.joueur = None
-        #
-        #             for pos_mur in posList:
-        #                 mur = self.map.get_at(*pos_mur)
-        #                 mur.cassable = None
-        #
-        #             del (self.projectiles[id])
-        #     if "shoot" in msg:
-        #         idjoueur = msg[2]
-        #         idproj = msg[3]
-        #         pos = msg[4]
-        #         dir = msg[5]
-        #         self.projectiles[idproj] = (pos, dir)
-        #
-        # if self.map is not None:
-        #     self.map.update(donnees)
-        #     joueur = self.map.get_joueur(self.idJoueur)
-        #
-        #     for id, (pos, dir) in self.projectiles.items():
-        #         coming = self.proj_coming((pos, dir), joueur, self.map)
-        #         print("coming", coming)
-        #         if coming > -1:
-        #             print("defense")
-        #             cost = cost_orientation(joueur.direction, self.calc_retourne(joueur, pos))
-        #             print("cost", cost)
-        #             if cost < coming:
-        #                 # retourne et tire
-        #                 rotate = rotate_to(cast_rot_inverse(joueur.direction),
-        #                                    rotation_requise(joueur.current_pos(), pos))
-        #
-        #                 print("rotate", rotate)
-        #                 if cost < 2:
-        #                     self.send_message([rotate, "shoot"])
-        #                 else:
-        #                     self.send_message([rotate])
-        #                 return
-        #
-        #     moi = self.map.get_joueur(self.idJoueur)
-        #     ma_pos = moi.current_pos()
-        #
-        #     dest = self.map.position_bonus()
-        #     if dest is None:
-        #         dest_joueurs = [pos for pos in self.map.position_joueurs() if pos is not None and pos != ma_pos]
-        #
-        #         if len(dest_joueurs) == 0:
-        #             dest = (self.map.bornes[0] // 2, self.map.bornes[1] // 2)
-        #         else:
-        #             dest = dest_joueurs[0]
-        #
-        #     print("not def ", dest)
-        #     path = aStar(self.map, ma_pos, dest)
-        #
-        #     if len(path) > 0:
-        #         print(path)
-        #         shoot = self.map.get_at(*path[0]).est_mur_traversable()
-        #         rot_req = rotation_requise(ma_pos, path[0])
-        #         rotation = rotate_to(cast_rot_inverse(moi.direction), rot_req)
-        #         if rotation is None:
-        #             if shoot:
-        #                 print("move, shoot")
-        #                 self.send_message(["move", "shoot"])
-        #             else:
-        #                 print("move")
-        #                 self.send_message(["move"])
-        #         else:
-        #             if shoot:
-        #                 print("rot, shoot")
-        #                 self.send_message([rotation])
-        #             else:
-        #                 print("move, shoot")
-        #                 self.send_message([rotation, "shoot"])
-
-    def calc_retourne(self, me, proj):
-        (x, y) = proj
-        (myx, myy) = me.current_pos()
-        if x == myx:
-            if y < myy:
-                return [0, -1]
-            else:
-                return [0, 1]
-        elif y == myy:
-            if x < myx:
-                return [-1, 0]
-            else:
-                return [1, 0]
-
-    def proj_coming(self, proj, me, map):
-        (pos, dir) = proj
-        (pos, dir) = proj
-        (myPos) = me.current_pos()
-        x = pos[0]
-        y = pos[1]
-
-        while True:
-            if x == myPos[0] and y == myPos[1]:
-                return (abs(myPos[0] - pos[0]) + abs(myPos[1] - pos[1])) / 2
-            if map.get_at(x, y).est_mur():
-                return -1
-            x += dir[0]
-            y += dir[1]
+        # action = self.liste_actions[self.state]
+        # self.state = (self.state + 1) % len(self.liste_actions)
+        # self.send_message(action)
+        to_send = self.game.play(donnees)
+        if to_send is not None:
+            self.send_message(to_send)
 
     def connection_lost(self, exc):
         self.loop.stop()
--- game.py
+++ game.py
@@ -32,6 +32,20 @@ class Joueur:
         except:
             print("WARNING : try to move before respawn")
 
+    def calc_next_move(self, goal):
+        (curx, cury) = tuple(self.current_pos)
+        (goalx, goaly) = tuple(goal)
+        (dirx, diry) = tuple(self.dir)
+        print((curx, cury), (dirx, diry), (goalx, goaly))
+        if (curx + dirx) == goalx and (cury + diry) == goaly:
+            return "move"
+        else:
+            goaldirx = goalx - curx
+            goaldiry = goaly - cury
+            rot_ord = calc_orientation((dirx, diry), (goaldirx, goaldiry))
+            print(rot_ord)
+            return rot_ord
+
 
 class Map:
     walls = []
@@ -70,19 +84,54 @@ class Map:
         else:
             print("WARNING : can't destroy wall", (x, y))
 
+    def is_destructible_wall(self, pos):
+        (x, y) = tuple(pos)
+        return self.map[x][y] == "C"
+
+    def is_indestrutible_wall(self, pos):
+        (x, y) = tuple(pos)
+        return self.map[x][y] == "I"
+
+    def is_not_wall(self, pos):
+        (x, y) = tuple(pos)
+        return self.map[x][y] == " "
+
+    def is_wall(self, pos):
+        return not self.is_not_wall(pos)
+
     def print(self):
         print((self.maxx, self.maxy))
         print(self.map)
 
+    def get_neighbors(self, pos):
+        (x, y) = pos
+        neigh = []
+        if not self.is_indestrutible_wall([x - 1, y]):
+            neigh.append((x - 1, y))
+        if not self.is_indestrutible_wall([x + 1, y]):
+            neigh.append((x + 1, y))
+        if not self.is_indestrutible_wall([x, y - 1]):
+            neigh.append((x, y - 1))
+        if not self.is_indestrutible_wall([x, y + 1]):
+            neigh.append((x, y + 1))
+        return neigh
+
 
 class Game:
-    myId = 0
+    my_id = 0
     joueurs = {}
     map = None
     bonus = []
     proj = {}
+    cible = None
 
     def del_bonus(self, posbonus):
+
+        if self.cible is not None:
+            (_, cible_pos) = self.cible
+            if posbonus == cible_pos:
+                self.cible = None
+
         to_remove = None
         for (points, pos) in self.bonus:
             if pos == posbonus:
@@ -107,11 +156,26 @@ class Game:
         for pos in murPosList:
             self.map.destroy_wall(pos)
 
+    def proj_incoming(self, my_pos, pos, dir) -> int:
+        (myx, myy) = tuple(my_pos)
+        (posx, posy) = tuple(pos)
+        (dirx, diry) = tuple(dir)
+        newx = posx
+        newy = posy
+        while True:
+            newx += dirx
+            newy += diry
+            if newx == myx and newy == myy:
+                return abs(posx - myx) + abs(posy - myy)
+            if self.map.is_wall([newx, newy]):
+                break
+        return -1
+
     def play(self, donnees):
         print("         NEW TURN            ")
         # print(donnees)
         if "idJoueur" in donnees:
-            myId = donnees["idJoueur"]
+            self.my_id = donnees["idJoueur"]
         elif "map" in donnees:
             # init map
             self.map = Map()
@@ -146,6 +210,7 @@ class Game:
                     elif action == "rotate":
                         id = play[2]
                         dir = play[3]
+                        print("update dir", dir)
                         self.joueurs[id].update_dir(dir)
                     elif action == "recupere_bonus":
                         id = play[2]
@@ -175,5 +240,96 @@ class Game:
                         tank_killed = play[5]
                         # ignore score
                         self.explode(idp, pos, murDone, tank_killed)
-        # jeu
-        return ["hrotate", "shoot"]
+
+            # jeu
+            my_player = self.joueurs[self.my_id]
+            # dest = None
+            if len(self.bonus) > 0:
+                self.bonus.sort(key=lambda x: a_star(self.map, my_player.current_pos, x[1]), reverse=True)
+                (points, pos_bonus) = self.bonus[0]
+                path = a_star(self.map, my_player.current_pos, pos_bonus)
+                if self.cible is None:
+                    self.cible = self.bonus[0]
+                else:
+                    (cible_points, cible_pos) = self.cible
+                    path_cible = a_star(self.map, my_player.current_pos, cible_pos)
+                    if (len(path) + 2) < len(path_cible):
+                        self.cible = self.bonus
+                    else:
+                        path = path_cible
+
+                return [my_player.calc_next_move(path[1])]
+            else:
+                return []
+
+
+from queue import PriorityQueue
+
+
+def a_star(graph, start, goal):
+    start, goal = tuple(start), tuple(goal)
+
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.get_neighbors(current):
+            new_cost = cost_so_far[current] + 1
+
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + 1
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    path = []
+    current = goal
+    while current is not None:
+        path.insert(0, current)
+        current = came_from[current]
+    return path
+
+
+def calc_orientation(cur_dir, goal_dir):
+    print(cur_dir, goal_dir)
+    hrot = "hrotate"
+    trot = "trotate"
+    if cur_dir == (0, 1):
+        if goal_dir == (1, 0):
+            return hrot
+        elif goal_dir == (-1, 0):
+            return trot
+        elif goal_dir == (0, -1):
+            return hrot
+    elif cur_dir == (0, -1):
+        if goal_dir == (1, 0):
+            return hrot
+        elif goal_dir == (-1, 0):
+            return trot
+        elif goal_dir == (0, 1):
+            return hrot
+    elif cur_dir == (1, 0):
+        if goal_dir == (0, 1):
+            return trot
+        elif goal_dir == (0, -1):
+            return hrot
+        elif goal_dir == (-1, 0):
+            return hrot
+    elif cur_dir == (-1, 0):
+        if goal_dir == (0, 1):
+            return trot
+        elif goal_dir == (0, -1):
+            return hrot
+        elif goal_dir == (1, 0):
+            return hrot
+    print("WARNING : invalid calc_orientation")
+    return
